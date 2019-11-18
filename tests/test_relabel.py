2c403feaa35d96be916e24125789e167c6bff128
monocongo <monocongo@gmail.com>
2019-11-18 13:46:07 -0500

    updates for the rename of rename.py to relabel.py; added docstring
    #6

--- tests/test_relabel.py
+++ tests/test_relabel.py
@@ -3,7 +3,7 @@ from xml.etree import ElementTree
 
 import pytest
 
-from cvdata import rename
+from cvdata import relabel
 
 # ------------------------------------------------------------------------------
 # disable logging messages
@@ -25,7 +25,7 @@ def test_relabel_pascal(
 
     # confirm that a relabeling won't occur if the old value is not present
     etree_before_relabel = ElementTree.parse(pascal_file_path)
-    rename.relabel_pascal(pascal_file_path, "NOT_PRESENT", "NOT_USED")
+    relabel.relabel_pascal(pascal_file_path, "NOT_PRESENT", "NOT_USED")
     etree_after_relabel = ElementTree.parse(pascal_file_path)
     assert elements_equal(
         etree_before_relabel.getroot(),
@@ -33,7 +33,7 @@ def test_relabel_pascal(
     )
 
     # confirm that relabeling occurred as expected
-    rename.relabel_pascal(pascal_file_path, "pistol", "firearm")
+    relabel.relabel_pascal(pascal_file_path, "pistol", "firearm")
     etree_after_relabel = ElementTree.parse(pascal_file_path)
     expected_pascal_file_name = "expected_pascal_1.xml"
     expected_pascal_file_path = str(data_dir.join(expected_pascal_file_name))
@@ -45,21 +45,31 @@ def test_relabel_pascal(
 
     # confirm that invalid argument types raise an error
     with pytest.raises(ValueError) as ex_info:
-        rename.relabel_pascal(None, "don't care", "don't care")
-        rename.relabel_pascal(pascal_file_path, None, "don't care")
-        rename.relabel_pascal(pascal_file_path, "don't care", None)
-        rename.relabel_pascal("/not/present", "don't care", "don't care")
-        rename.relabel_pascal(1, "don't care", "don't care")
-        rename.relabel_pascal(1.0, "don't care", "don't care")
-        rename.relabel_pascal(pascal_file_path, 1, "don't care")
-        rename.relabel_pascal(pascal_file_path, 1.0, "don't care")
-        rename.relabel_pascal(pascal_file_path, True, "don't care")
-        rename.relabel_pascal(pascal_file_path, "don't care", 1)
-        rename.relabel_pascal(pascal_file_path, "don't care", 1.0)
-        rename.relabel_pascal(pascal_file_path, "don't care", True)
+        relabel.relabel_pascal(None, "don't care", "don't care")
+        relabel.relabel_pascal(pascal_file_path, None, "don't care")
+        relabel.relabel_pascal(pascal_file_path, "don't care", None)
+        relabel.relabel_pascal("/not/present", "don't care", "don't care")
+        relabel.relabel_pascal(1, "don't care", "don't care")
+        relabel.relabel_pascal(1.0, "don't care", "don't care")
+        relabel.relabel_pascal(pascal_file_path, 1, "don't care")
+        relabel.relabel_pascal(pascal_file_path, 1.0, "don't care")
+        relabel.relabel_pascal(pascal_file_path, True, "don't care")
+        relabel.relabel_pascal(pascal_file_path, "don't care", 1)
+        relabel.relabel_pascal(pascal_file_path, "don't care", 1.0)
+        relabel.relabel_pascal(pascal_file_path, "don't care", True)
 
 
+# ------------------------------------------------------------------------------
 def elements_equal(e1, e2):
+    """
+    Utility function to compare Element objects.
+
+    From https://stackoverflow.com/a/24349916/85248
+
+    :param e1:
+    :param e2:
+    :return:
+    """
     if e1.tag != e2.tag: return False
     if e1.text != e2.text: return False
     if e1.tail != e2.tail: return False
