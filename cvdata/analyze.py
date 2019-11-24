571417bdcf128afb3acc7adf3a7e42d5e21b2c57
monocongo <monocongo@gmail.com>
2019-11-24 16:38:06 -0500

    added an output to display the image counts per label
    #30

--- cvdata/analyze.py
+++ cvdata/analyze.py
@@ -206,3 +206,8 @@ if __name__ == "__main__":
                     label_counts[label] += 1
                 else:
                     label_counts[label] = 1
+
+        pass
+
+        for label, count in label_counts.items():
+            print(f"Label: {label}\t\tCount: {count}")
