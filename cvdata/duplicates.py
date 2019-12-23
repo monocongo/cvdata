d115cf19179ead88cf36c454e152e97393b75511
James Adams <monocongo@gmail.com>
2019-12-23 13:20:31 -0400

    fleshed out the function to find duplicates, still need to test and add section for deleting/moving the corresponding annotation file
    #66

--- cvdata/duplicates.py
+++ cvdata/duplicates.py
@@ -1,7 +1,11 @@
 import argparse
 import logging
 import os
+import shutil
 from typing import List
+
+import imagehash
+from PIL import Image
 
 
 # ------------------------------------------------------------------------------
@@ -14,13 +18,54 @@ logging.basicConfig(
 _logger = logging.getLogger(__name__)
 
 
+# ------------------------------------------------------------------------------
 def remove_duplicates(
         images_dir: str,
         annotations_dir: str = None,
         duplicates_dir: str = None,
 ) -> List[str]:
 
-    pass
+    # create the duplicates directory in case it doesn't yet exist
+    if duplicates_dir is not None:
+        os.makedirs(duplicates_dir, exist_ok=True)
+
+    # build a set of image hashes and a list of IDs that are duplicates
+    image_hashes = set()
+    duplicate_ids = []
+    for image_file_name in os.listdir(images_dir):
+
+        # only process JPG images
+        if not image_file_name.endswith(".jpg"):
+            continue
+
+        # get a hash of the image and add the image ID to the list of duplicates
+        # if it's already been added, otherwise add it to the set of hashes
+        image = Image.open(os.path.join(images_dir, image_file_name))
+        image_id = os.path.splitext(image_file_name)[0]
+        image_hash = imagehash.dhash(image)
+        if image_hash in image_hashes:
+            duplicate_ids.append(image_id)
+        else:
+            image_hashes.add(image_hash)
+
+    duplicate_files = []
+    for duplicate_id in duplicate_ids:
+
+        image_file_name = duplicate_id + ".jpg"
+        image_path = os.path.join(images_dir, image_file_name)
+        duplicate_files.append(image_path)
+
+        # either move or delete the image file
+        if duplicates_dir is None:
+            os.remove(image_path)
+        else:
+            shutil.move(image_path, os.path.join(duplicates_dir, image_file_name))
+
+        if annotations_dir is not None:
+            # TODO
+            pass
+
+    return duplicate_files
 
 
 # ------------------------------------------------------------------------------
