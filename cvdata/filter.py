9c9d11c59f69477306764995eb6aeaf1e14ec7ae
James Adams <monocongo@gmail.com>
2020-01-01 12:04:58 -0400

    added functions for counting the boxes per label in Darknet and KITTI annotation files
    #93

--- cvdata/filter.py
+++ cvdata/filter.py
@@ -8,6 +8,77 @@ from cvdata.utils import matching_ids
 
 
 # ------------------------------------------------------------------------------
+def _count_boxes_darknet(
+        darknet_file_path: str,
+        darknet_labels_path: str,
+) -> Dict:
+    """
+    TODO
+
+    :param darknet_file_path:
+    :param darknet_labels_path:
+    :return: dictionary with labels mapped to their bounding box counts
+    """
+
+    # read the Darknet labels into a dictionary mapping label to label index
+    label_indices = darknet_labels_to_indices(darknet_labels_path)
+
+    box_counts = {}
+    with open(darknet_file_path) as darknet_file:
+        for bbox_line in darknet_file:
+            parts = bbox_line.split()
+            label_index = int(parts[0])
+            if label_index not in label_indices:
+                raise ValueError(
+                    f"Unexpected label index ({label_index} found in Darknet "
+                    f"annotation file {darknet_file_path}, incompatible with "
+                    f"the Darknet labels file {darknet_labels_path}")
+            else:
+                label = label_indices[label_index]
+                if label in box_counts:
+                    box_counts[label] = box_counts[label] + 1
+                else:
+                    box_counts[label] = 1
+
+    return box_counts
+
+
+# ------------------------------------------------------------------------------
+def _count_boxes_kitti(
+        kitti_file_path: str,
+) -> Dict:
+    """
+    TODO
+
+    :param kitti_file_path:
+    :return:
+    """
+
+    box_counts = {}
+    with open(kitti_file_path) as kitti_file:
+        for bbox_line in kitti_file:
+            parts = bbox_line.split()
+            label = int(parts[0])
+            if label in box_counts:
+                box_counts[label] = box_counts[label] + 1
+            else:
+                box_counts[label] = 1
+
+    return box_counts
+
+
+# ------------------------------------------------------------------------------
+def count_boxes(annotation_file_path, annotation_format, darknet_labels_path):
+
+    if annotation_format == "darknet":
+        return _count_boxes_darknet(annotation_file_path, darknet_labels_path)
+    elif annotation_format == "kitti":
+        return _count_boxes_kitti(annotation_file_path)
+    else:
+        raise ValueError(f"Unsupported annotation format: {annotation_format}")
+
+
+# ------------------------------------------------------------------------------
 def filter_class_boxes(
         src_images_dir: str,
         src_annotations_dir: str,
@@ -15,6 +86,7 @@ def filter_class_boxes(
         dest_annotations_dir: str,
         class_counts: Dict,
         annotation_format: str,
+        darknet_labels_path = None,
 ):
     """
     TODO
@@ -25,6 +97,7 @@ def filter_class_boxes(
     :param dest_annotations_dir:
     :param class_counts:
     :param annotation_format:
+    :param darknet_labels_path: path to the labels file corresponding to Darknet
     """
 
     # make sure we don't have the same directories for src and dest
@@ -50,6 +123,9 @@ def filter_class_boxes(
     os.makedirs(dest_images_dir, exist_ok=True)
     os.makedirs(dest_annotations_dir, exist_ok=True)
 
+    # keep a count of the boxes we've processed for each image class type
+    processed_class_counts = {k: 0 for k in class_counts.keys()}
+
     # get all file IDs for image/annotation file matches
     file_ids = \
         matching_ids(
@@ -59,16 +135,20 @@ def filter_class_boxes(
             image_ext,
         )
 
-    # keep a count of the boxes we've processed for each image class type
-    processed_class_counts = {k: 0 for k in class_counts.keys()}
-
+    # loop over all the possible image/annotation file pairs
     for file_id in file_ids:
 
+        # only include the file if we find a box for one of the specified labels
         include_file = False
+
+        # list of any box labels found that aren't in list of specified labels
         remove_labels = []
+
+        # get the count(s) of boxes per class label
         annotation_file_name = file_id + annotation_ext
         src_annotation_path = os.path.join(src_annotations_dir, annotation_file_name)
-        box_counts = count_boxes(src_annotation_path, annotation_format)
+        box_counts = count_boxes(src_annotation_path, annotation_format, darknet_labels_path)
+
         for label in box_counts.keys():
             if label in class_counts:
                 processed_class_count = processed_class_counts[label]
