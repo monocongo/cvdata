e03f6948df835b6702a500e4527482817ea3d7ad
James Adams <monocongo@gmail.com>
2020-01-27 14:30:25 -0500

    replaced the TFOD API functions *_feature with modified local copies
    #120

--- cvdata/convert.py
+++ cvdata/convert.py
@@ -10,7 +10,6 @@ from xml.etree import ElementTree
 
 import contextlib2
 import cv2
-from object_detection.utils import dataset_util
 import pandas as pd
 from PIL import Image
 import tensorflow as tf
@@ -119,6 +118,46 @@ def _dataset_bbox_examples(
 
 
 # ------------------------------------------------------------------------------
+def _int64_feature(
+        value: int,
+) -> tf.train.Feature:
+
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+# ------------------------------------------------------------------------------
+def _int64_list_feature(
+        value: List[int],
+) -> tf.train.Feature:
+
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+# ------------------------------------------------------------------------------
+def _bytes_feature(
+        value: str,
+) -> tf.train.Feature:
+
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+# ------------------------------------------------------------------------------
+def _bytes_list_feature(
+        value: List[str],
+) -> tf.train.Feature:
+
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+# ------------------------------------------------------------------------------
+def _float_list_feature(
+        value: List[float],
+) -> tf.train.Feature:
+
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+# ------------------------------------------------------------------------------
 def _create_tf_example(
         label_indices: Dict,
         group: NamedTuple,
@@ -162,18 +201,18 @@ def _create_tf_example(
 
     # build the Example from the lists of coordinates, class labels/indices, etc.
     tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(img_bytes),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
+        'image/height': _int64_feature(height),
+        'image/width': _int64_feature(width),
+        'image/filename': _bytes_feature(filename),
+        'image/source_id': _bytes_feature(filename),
+        'image/encoded': _bytes_feature(img_bytes),
+        'image/format': _bytes_feature(image_format),
+        'image/object/bbox/xmin': _float_list_feature(xmins),
+        'image/object/bbox/xmax': _float_list_feature(xmaxs),
+        'image/object/bbox/ymin': _float_list_feature(ymins),
+        'image/object/bbox/ymax': _float_list_feature(ymaxs),
+        'image/object/class/text': _bytes_list_feature(classes_text),
+        'image/object/class/label': _int64_list_feature(classes),
     }))
 
     return tf_example
